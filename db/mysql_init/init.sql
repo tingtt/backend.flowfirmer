756f418a97f6e22cd532ca96632a42b64fbb7d09
Kantaro0829 <eshangwan@gmail.com>
2021-05-29 06:48:48 +0900

    modfied file

--- db/mysql_init/init.sql
+++ db/mysql_init/init.sql
@@ -1,14 +1,7 @@
-DROP SCHEMA IF EXISTS test;
-CREATE SCHEMA test;
+CREATE DATABASE IF NOT EXISTS test;
+
+--CREATE SCHEMA test;
 USE test;
 
 DROP TABLE IF EXISTS user;
 
-CREATE TABLE user
-(
-  id           INT(10),
-  name     VARCHAR(40)
-);
-
-INSERT INTO employee (id, name) VALUES (1, "Nagaoka");
-INSERT INTO employee (id, name) VALUES (2, "Tanaka");
